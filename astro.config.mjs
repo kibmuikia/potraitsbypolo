7ffc3c625ef1c60a3f2157be31208d6fce0a1443
Kibuthi Allan <muikiaa@gmail.com>
2026-05-05 15:16:17 +0300

    fix(config): sort imports in astro.config.mjs.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,9 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import compress from "@playform/compress";
 import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 import AutoImport from "astro-auto-import";
 import icon from "astro-icon";
-import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
